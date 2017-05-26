73a0c221f3bff7692f78a0c75aa115ddb7b5c3f1
Daniel Pereira <dpereira@lifesize.com>
2017-05-26 14:40:30 -0500

    Add code to reproduce the error

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const electron = require('electron');
+
+const desktopCapturerOptions = {
+  thumbnailSize: { width: 184, height: 104 },
+  types: ['window', 'screen']
+};
+
+const sourcesDiv = document.getElementById('sources');
+
+function displaySources() {
+  electron.desktopCapturer.getSources(desktopCapturerOptions, (error, sources) => {
+    if (error)
+      throw error;
+    sourcesDiv.innerHTML += sources.map(s => s.name).join(', ') + '<br />';
+    setTimeout(displaySources, 1000);
+  });
+}
+
+setTimeout(displaySources, 1000);
